79f4e0172d9230776b5fbe309a5b5b1338e6589d
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-11-26 15:51:04 -0600

    updated about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ import '../../css/about.css'
 // We assign them to their own variable names
 export default function About() {
     return (
-        <div className='container'>
+        <div className=''>
             <div>
                 <h2>
                     About Me
@@ -17,14 +17,15 @@ export default function About() {
                         <p>
                             Hi, my name is Ali Celikay and I am a web developer that lives in a Chicago suborb, Des Plaines. I recently graduated from a Full Stack Flex coding bootcamp with Northwestern University where I earned my certificate of completion.
 
+                            During my bootcamp experience, I was mentored by Full Stack graduates to work with like minded individuals on projects where we successfully deployed professional production ready websites. On one of these projects, I was chosen to be the project manager where I encouraged an environment of communication for tasks, problems, and roles and we earned a 90% as a grade on that project.
+                        </p>
+                        <p>
                             In my free time, I am working on completing my Computer Science Bachlears degree. I recently graduated from William Rainey Harper College which is a community college at Schamburg Illinois.
 
                             I plan to continue my education at University of Illinois Chicago where I plan to earn my Bachlears degree in Computer Science.
-                        </p>
-                        <p>
+
                             During my college years, I have completed a few NASA programs where I was mentored by NASA engineers to work with like minded individuals on projects to successfully execute extraterrestrial missions. On one of these projects, I was chosen to be a business team lead where I successfully budgeted all the travel, material, and salary costs to be under our budget.
 
-                            During my bootcamp program, I was mentored by Full Stack graduates to work with like minded individuals on projects where we successfully deployed professional production ready websites. On one of these projects, I was chosen to be the project manager where I encouraged an environment of communication for tasks, problems, and roles and we earned a 90% as a grade on that project.
                         </p>
                     </div>
                     <div className='aside'>
